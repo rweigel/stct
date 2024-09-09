9f2293ce78c60b0e1ae197c369964c6d32de93f3
Bob Weigel <Robert.S.Weigel@gmail.com>
2024-09-09 15:04:15 -0400

    lists

--- GEO.md
+++ GEO.md
@@ -1,3 +1,4 @@
 GEO
+https://www.iers.org/SharedDocs/Publikationen/EN/IERS/Publications/tn/TechnNote36/tn36_031.pdf?__blob=publicationFile&v=1
 
 https://docs.astropy.org/en/stable/_modules/astropy/coordinates/earth.html
--- lists/combine.py
+++ lists/combine.py
@@ -1,27 +1,29 @@
+import os
 import csv
+import glob
+import json
 
-def read_csv(filename, sourceLabel, source):
+def read_csv(filename, sourceLabel):
   D = []
   print("Reading", filename)
   with open(filename, newline='') as csvfile:
-      reader = csv.reader(csvfile)
-      for row in reader:
-        if row[0].startswith('#'):
-          continue
+    reader = csv.reader(csvfile)
+    header = next(reader)
+    source_url = header[0].split("#")[1].strip()
+    source_file = filename
+    for row in reader:
+      d = {"term": "", "label": "", "definition": "", "sourceLabel": sourceLabel, "source_url": source_url, "source_file": source_file}
+      d["term"] = row[0].replace(" ","")
+      if len(row) > 1:
+        d["label"] = row[1]
+      if len(row) > 2:
+        d["definition"] = row[2]
 
-        d = {"term": "", "label": "", "definition": "", "sourceLabel": sourceLabel, "source": source}
-
-        d["term"] = row[0].replace(" ","")
-        if len(row) > 1:
-          d["label"] = row[1]
-        if len(row) > 2:
-          d["definition"] = row[2]
-
-        D.append(d)
+      D.append(d)
 
   return D
 
-def read_xml(filename, sourceLabel, source):
+def read_xml(filename, sourceLabel, source_url):
   import xmltodict
 
   print("Reading", filename)
@@ -34,79 +36,54 @@ def read_xml(filename, sourceLabel, source):
          'label': frame['fullname'],
          'definition': frame['description'],
          'sourceLabel': sourceLabel,
-         'source': source
+         'source_url': source_url,
+         'source_file': filename
     }
     D.append(d)
 
   return D
 
-def read_md():
-  # Old code. Not used anymore.
-  with open('VEP-for-IVOA.md') as mdfile:
-    line = None
-    d = {}
-    while line != '':
-      line = mdfile.readline()
-      if line.startswith('**Term**: '):
-        d["term"] = line.replace("**Term**: ","").rstrip().replace('#','').replace('`','').rstrip()
-      if line.startswith('**Label**: '):
-        d["label"] = line.replace("**Label**: ","").replace('#','').replace('`','').rstrip()
-
-      if line.startswith('**Definition**:'):
-        d["source"] = "https://gitlab.irap.omp.eu/CDPP/TREPS/blob/master/server/kernel/data/frames.xml"
-        d["definition"] = ""
-        while True:
-          line = mdfile.readline()
-          if line.startswith('____'):
-            D.append(d)
-            break
-          d["definition"] += line
-
-
-D1 = read_csv('documents/IVOA-2007-STCT.csv', 'IVOA', 'https://www.ivoa.net/documents/REC/DM/STC-20071030.html#_Toc181531804')
-D  = read_csv('documents/ivoa-refframe-2022-02-22-terms.csv', 'https://github.com/ivoa-std/Vocabularies/blob/master/refframe/terms.csv')
-D6 = read_xml('documents/frames.xml','TREPS', 'https://gitlab.irap.omp.eu/CDPP/TREPS/blob/master/server/kernel/data/frames.xml')
-D2 = read_csv('documents/cdpp-adma-request.csv', 'CDPP/AMDA', 'Request sent to SPASE email list')
-D3 = read_csv('documents/spase.csv', 'SPASE', 'https://spase-group.org/data/model/spase-2.4.0/spase-2_4_0_xsd.html#CoordinateSystemName')
-
-D4 = read_csv('software/astropy.csv', 'AstroPy', 'https://docs.astropy.org/en/stable/coordinates/skycoord.html')
-D4 = read_csv('software/Kamodo.csv', 'Kamodo', 'https://nasa.github.io/Kamodo/notebooks/CoordinateConversions.html#retrieve-a-real-satellite-trajectory-as-input')
-D4 = read_csv('software/sunpy.csv', 'SunPy', 'https://github.com/sunpy/sunpy/wiki/Coordinate-systems')
-D4 = read_csv('software/spacepy.csv', 'SpacePy', 'https://spacepy.github.io/autosummary/spacepy.coordinates.Coords.html')
-
-D7 = read_csv('software/spice-built-in-pck-body-fixed.csv', 'NAIF', 'https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/frames.html#Appendix.%20%60%60Built%20in''%20Inertial%20Reference%20Frames')
-
-D5 = read_csv('papers/laundal-and-richmond-2016.csv', 'Laundal', 'https://doi.org/10.1007/s11214-016-0275-y')
-
-D = [*D1, *D2, *D3, *D4, *D5, *D6, *D7]
+data = []
+files = glob.glob('**/*.csv') + glob.glob('**/*.xml')
+for file in files:
+  file_name = file.split('/')[1]
+  file_id = file_name.split('-')[0]
+  file_ext = os.path.splitext(file_name)[1]
+  if file_ext == '.csv':
+    source_data = read_csv(file, file_id)
+  else:
+    source = "https://gitlab.irap.omp.eu/CDPP/TREPS/blob/master/server/kernel/data/frames.xml"
+    source_data = read_xml(file, file_id, source)
+  data = [*data, *source_data]
 
 # https://stackoverflow.com/a/21004113
-D = sorted(D, key=lambda x: x['term'])
+data = sorted(data, key=lambda x: x['term'])
 
-import json
 filename = "combined.json"
 with open(filename, "w") as outfile:
   print("Writing", filename)
-  json.dump(D, outfile, indent=2)
+  json.dump(data, outfile, indent=2)
 
 filename = "combined.csv"
 with open(filename, "w") as outfile:
   print("Writing", filename)
-  outfile.write("term,label,definition,sourceLabel,source\n")
-  for d in D:
-    outfile.write(f'{d["term"]},"{d["label"]}","{d["definition"]}",{d["sourceLabel"]},"{d["source"]}"\n')
+  outfile.write("term,label,definition,sourceLabel,source_url,source_file\n")
+  for d in data:
+    outfile.write(f'{d["term"]},"{d["label"]}","{d["definition"]}",{d["sourceLabel"]},"{d["source_url"]}","{d["source_file"]}"\n')
 
-filename = "../VEP-for-IVOA.head.md"
-with open(filename,"r") as f:
-  print("Reading", filename)
-  head = f.read()
 
-filename = "../VEP-for-IVOA.md"
-with open(filename, "w") as outfile: 
-  print("Writing", filename)
-  outfile.write(head+"\n")
-  for d in D:
-    outfile.write(f'**Term**: `#{d["term"]}`\n')
-    outfile.write(f'**Label**: {d["label"]}\n')
-    outfile.write(f'**Definition**: \n{d["definition"]}\n')
-    outfile.write(f'______________________________________________________________\n\n')
+if False:
+  filename = "../VEP-for-IVOA.head.md"
+  with open(filename,"r") as f:
+    print("Reading", filename)
+    head = f.read()
+
+  filename = "../VEP-for-IVOA.md"
+  with open(filename, "w") as outfile: 
+    print("Writing", filename)
+    outfile.write(head+"\n")
+    for d in D:
+      outfile.write(f'**Term**: `#{d["term"]}`\n')
+      outfile.write(f'**Label**: {d["label"]}\n')
+      outfile.write(f'**Definition**: \n{d["definition"]}\n')
+      outfile.write(f'______________________________________________________________\n\n')
